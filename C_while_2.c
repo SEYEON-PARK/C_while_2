e39bfed0c586215359aa19212a8f9929c821b746
SEYEON_PARK <fourleaf123@naver.com>
2020-11-26 00:24:11 +0900

    C언어 while문 연습!(2)

---
+++ C_while_2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void)
+{
+	char name[30];
+	printf("이름을 입력하세요. : ");
+	scanf_s("%s", name, sizeof(name));
+
+	int i = 0;
+	while (name[i]!=NULL)
+	{
+		printf("%c", name[i]);
+		i++;
+	}
+	printf("\n");
+	printf("%s", name);
+
+	return 0;
+}
